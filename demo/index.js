be2a695f998121238de64cea889f3aab10172483
yukap <jingweirong@mistong.com>
2018-09-26 11:46:55 +0800

    ci: add placeholder for demo, demo in npm

--- demo/index.js
+++ demo/index.js
@@ -1,20 +1,31 @@
+/*
+* annotation like `placeholder begin *** and
+* placeholder end ***` is required for generate eui manual, please keep it
+*/
 import React from 'react';
 import ReactDOM from 'react-dom';
-import EmptyData from '../src/index';
 
-import './demo.scss';
+/* placeholder begin src */
+import EmptyData from '../src';
+/* placeholder end src */
 
+import './index.scss';
+
+/* placeholder begin class */
 class Demo extends React.Component {
   render() {
     return (
-      <div className="demo">
+      <div className="demo eui-empty-data-demo">
         <EmptyData style={{ width: '200px' }} />
       </div>
     );
   }
 }
+/* placeholder end class */
 
+/* placeholder begin ReactDOM */
 ReactDOM.render(
   <Demo />,
   document.getElementById('app'),
 );
+/* placeholder end ReactDOM */
